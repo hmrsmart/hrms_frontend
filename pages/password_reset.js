46c6b47d9c3d7109e8079aa02953db5e289fc536
skanjarla <skanjarla@zyclyx.com>
2020-10-05 16:11:47 +0530

    Add password reset

--- pages/password_reset.js
+++ pages/password_reset.js
@@ -14,135 +14,38 @@ import { login } from "../lib/auth";
 import AppContext from "../context/AppContext";
 
 function PasswordReset(props) {
-  const [data, updateData] = useState({ identifier: "", password: "" });
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
   const router = useRouter();
   const appContext = useContext(AppContext);
+  const { user, setUser } = useContext(AppContext);
 
   useEffect(() => {
-    if (appContext.isAuthenticated) {
-      router.push("/"); // redirect if you're already logged in
-    }
-  }, []);
+    var formdata = new FormData();
+    formdata.append("email", "skanjarla@zyclyx.com");
 
-  function onChange(event) {
-    updateData({ ...data, [event.target.name]: event.target.value });
-  }
+    var requestOptions = {
+      method: "POST",
+      body: formdata,
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://admin-zyclyx.herokuapp.com/auth/forgot-password",
+      requestOptions
+    )
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
+  }, []);
 
   return (
     <Container>
-      <Row className="d-flex justify-content-center align-items-center">
-        <Col sm="12" md={{ size: 6 }}>
-          <div className="paper p-4 bg-white">
-            <div className="py-2">
-              <h3 className="pb-3 login-title text-center">
-                Welcome to ZYCLYX Employee Portal
-              </h3>
-            </div>
-            <section className="wrapper">
-              {Object.entries(error).length !== 0 &&
-                error.constructor === Object &&
-                error.message.map((error) => {
-                  return (
-                    <div
-                      key={error.messages[0].id}
-                      style={{ marginBottom: 10 }}
-                    >
-                      <small style={{ color: "red" }}>
-                        {error.messages[0].message}
-                      </small>
-                    </div>
-                  );
-                })}
-              <Form>
-                <fieldset disabled={loading}>
-                  <FormGroup>
-                    <Label>Email:</Label>
-                    <Input
-                      onChange={(event) => onChange(event)}
-                      name="identifier"
-                      style={{ height: 55, fontSize: "1.2em" }}
-                    />
-                  </FormGroup>
-                  <FormGroup style={{ marginBottom: 30 }}>
-                    <Label>Password:</Label>
-                    <Input
-                      onChange={(event) => onChange(event)}
-                      type="password"
-                      name="password"
-                      style={{ height: 55, fontSize: "1.2em" }}
-                    />
-                  </FormGroup>
-
-                  <FormGroup>
-                    <span>
-                      <a href="">
-                        <small>Forgot Password?</small>
-                      </a>
-                    </span>
-                    <Button
-                      style={{ float: "right", width: 120 }}
-                      color="primary"
-                      onClick={() => {
-                        setLoading(true);
-                        login(data.identifier, data.password)
-                          .then((res) => {
-                            setLoading(false);
-                            // set authed User in global context to update header/app state
-                            appContext.setUser(res.data.user);
-                          })
-                          .catch((error) => {
-                            setError(error.response.data);
-                            setLoading(false);
-                          });
-                      }}
-                    >
-                      {loading ? "Loading... " : "Login"}
-                    </Button>
-                  </FormGroup>
-                </fieldset>
-              </Form>
-            </section>
-          </div>
-        </Col>
-      </Row>
-      <style jsx>
-        {`
-          .paper {
-            border: 1px solid lightgray;
-            box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
-              0px 1px 1px 0px rgba(0, 0, 0, 0.14),
-              0px 2px 1px -1px rgba(0, 0, 0, 0.12);
-            border-radius: 6px;
-            margin-top: 90px;
-            border-top: 2px solid #2196f3;
-          }
-          .notification {
-            color: #ab003c;
-          }
-          .header {
-            width: 100%;
-            height: 120px;
-            background-color: #2196f3;
-            margin-bottom: 30px;
-            border-radius-top: 6px;
-          }
-          .wrapper {
-            padding: 10px 30px 20px 30px !important;
-          }
-          a {
-            color: blue !important;
-          }
-          img {
-            margin: 15px 30px 10px 50px;
-          }
-          .login-title {
-            color: #2196f3;
-            font-size: 1.3rem;
-          }
-        `}
-      </style>
+      <div className="py-4">
+        <h2 className="text-center">Account Password Reset</h2>
+        <p className="text-center py-3">
+          Please check your Email - <strong>{user.email}</strong> for the
+          instructions to reset your ZYCLYX-HRMS Account password
+        </p>
+      </div>
     </Container>
   );
 }
