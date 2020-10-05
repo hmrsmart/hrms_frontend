93585c07fc7c455b44dc4049bc34dab9fb55c2ae
skanjarla <skanjarla@zyclyx.com>
2020-10-05 13:00:09 +0530

    Add payslips

--- pages/payslips.js
+++ pages/payslips.js
@@ -1,14 +1,11 @@
 import React, { useEffect, useContext, useState } from "react";
-import Link from "next/link";
-import { useForm } from "react-hook-form";
 import Cookie from "js-cookie";
 import { useRouter } from "next/router";
 import AppContext from "../context/AppContext";
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import { getDay } from "../lib/date";
 
-const Payslips = (props) => {
-  const [tasks, setTasks] = useState([]);
-
+const Payslips = () => {
+  const [payslips, setPaySlips] = useState([]);
   const { user, setUser } = useContext(AppContext);
 
   const appContext = useContext(AppContext);
@@ -18,11 +15,62 @@ const Payslips = (props) => {
     if (!appContext.isAuthenticated) {
       router.push("/login"); // redirect if you're not logged in
     }
+    const token = Cookie.get("token");
+    if (token) {
+      fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/payslips?user.username=${user.username}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setPaySlips(data);
+          console.log(data);
+        });
+    }
   }, []);
 
   return (
     <div className="container-fluid px-5 ml-4 d-flex justify-conent-center flex-column">
-      <h2 className="py-3">Payslips</h2>
+      <h2 className="py-3">Holidays List</h2>
+
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">SNO</th>
+            <th scope="col">Month & Year</th>
+            <th scope="col">PaySlip</th>
+          </tr>
+        </thead>
+        <tbody>
+          {payslips &&
+            payslips.map((payslip, index) => {
+              return (
+                <tr>
+                  <th scope="row">{index + 1}</th>
+                  <td>
+                    {payslip.Month} - {payslip.Year}
+                  </td>
+                  <td>
+                    {" "}
+                    <a
+                      href={payslip.Payslip[0].url}
+                      download="download"
+                      target="__blank"
+                    >
+                      Download
+                    </a>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
     </div>
   );
 };
