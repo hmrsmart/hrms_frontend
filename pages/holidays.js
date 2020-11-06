44f6462c3eff614b0ba06bf9294f30e3a347cfd6
skanjarla <skanjarla@zyclyx.com>
2020-11-06 17:37:17 +0530

    Update table design

--- pages/holidays.js
+++ pages/holidays.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useContext, useState } from "react";
 import Cookie from "js-cookie";
+import { Spinner } from "reactstrap";
 import { useRouter } from "next/router";
 import AppContext from "../context/AppContext";
 import { getDay } from "../lib/date";
@@ -32,7 +33,6 @@ const Holidays = () => {
         .then((data) => {
           setHolidays(data);
           setLoading(false);
-          console.log(data);
         })
         .catch((error) => {
           console.log(error);
@@ -42,9 +42,9 @@ const Holidays = () => {
 
   return (
     <div className="container-fluid d-flex justify-conent-center flex-column">
-      <h2 className="py-3">Holidays List</h2>
+      <h2 className="py-3 title-text">Holidays List</h2>
 
-      <table className="table table-striped">
+      <table className="table">
         <thead>
           <tr>
             <th scope="col">SNO</th>
@@ -58,15 +58,20 @@ const Holidays = () => {
           {isLoading && (
             <tr>
               <td colSpan="5" className="text-center py-5">
-                Loading . . .
+                <Spinner color="success" />
               </td>
             </tr>
           )}
           {holidays &&
             holidays.map((task, index) => {
               return (
-                <tr key={task.Ocation}>
-                  <th scope="row">{index + 1}</th>
+                <tr
+                  key={task.Ocation}
+                  className={`${
+                    task.Holiday_Type === "Mandatory" ? "mandatory" : "optional"
+                  }`}
+                >
+                  <td scope="row">{index + 1}</td>
                   <td>{task.Date}</td>
                   <td>{getDay(task.Date)}</td>
                   <td>{task.Occasion}</td>
@@ -76,6 +81,22 @@ const Holidays = () => {
             })}
         </tbody>
       </table>
+      <style jsx>
+        {`
+          th {
+            color: #00a14b;
+          }
+          td {
+            color: #454545;
+          }
+          .mandatory {
+            background-color: #a8d7a8;
+          }
+          .optional {
+            background-color: #cbe7cb;
+          }
+        `}
+      </style>
     </div>
   );
 };
