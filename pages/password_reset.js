d0ecea42a8147097ef76b8714e49a3abba073e24
skanjarla <skanjarla@zyclyx.com>
2020-11-06 17:38:32 +0530

    Update password reset

--- pages/password_reset.js
+++ pages/password_reset.js
@@ -1,41 +1,93 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useRouter } from "next/router";
-import { Container } from "reactstrap";
+import { useForm } from "react-hook-form";
+import { Container, Row, Col, Button, FormGroup, Label } from "reactstrap";
 import { login } from "../lib/auth";
 import AppContext from "../context/AppContext";
 
 function PasswordReset(props) {
-  const router = useRouter();
-  const appContext = useContext(AppContext);
-  const { user, setUser } = useContext(AppContext);
+  // const router = useRouter();
+  // const appContext = useContext(AppContext);
+  // const { user, setUser } = useContext(AppContext);
+  const [resetSuccessful, setResetSuccessful] = useState(false);
+  const { register, handleSubmit, errors } = useForm();
+  const [userEmail, setUserEmail] = useState();
+  const [loading, setLoading] = useState(false);
+  // useEffect(() => {
+  // var formdata = new FormData();
+  // formdata.append("email", user.email);
+  // var requestOptions = {
+  //   method: "POST",
+  //   body: formdata,
+  //   redirect: "follow",
+  // };
+  // fetch(
+  //   `${process.env.NEXT_PUBLIC_API_URL}/auth/forgot-password`,
+  //   requestOptions
+  // )
+  //   .then((response) => response.text())
+  //   .then((result) => console.log(result))
+  //   .catch((error) => console.log("error", error));
+  //}, []);
 
-  useEffect(() => {
+  const onSubmit = (data) => {
+    setUserEmail(data.identifier);
+    setLoading(true);
     var formdata = new FormData();
-    formdata.append("email", user.email);
-
+    formdata.append("email", data.identifier);
     var requestOptions = {
       method: "POST",
       body: formdata,
       redirect: "follow",
     };
-
     fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/auth/forgot-password`,
       requestOptions
     )
-      .then((response) => response.text())
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        setResetSuccessful(true);
+        setLoading(false);
+        return response.json();
+      })
       .then((result) => console.log(result))
       .catch((error) => console.log("error", error));
-  }, []);
-
+  };
   return (
-    <Container>
-      <div className="py-4">
-        <h2 className="text-center">Account Password Reset</h2>
-        <p className="text-center py-3">
-          Please check your Email - <strong>{user.email}</strong> for the
-          instructions to reset your ZYCLYX-HRMS Account password
-        </p>
+    <Container className="h-100 d-flex align-items-center justify-content-center">
+      <div className="py-5">
+        <h4 className="text-center">Account Password Reset</h4>
+
+        {!resetSuccessful ? (
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="d-flex flex-column align-items-center justify-content-center py-3"
+          >
+            <FormGroup>
+              <Label>Email</Label>
+              <input
+                type="email"
+                name="identifier"
+                className="form-control"
+                style={{ height: 55, fontSize: "1.2em" }}
+                ref={register({ required: true })}
+              />
+              {errors.identifier && (
+                <span className="err-msg">* Email is required</span>
+              )}
+            </FormGroup>
+            <button type="submit" className="btn btn-primary">
+              {!loading ? "Reset Password" : "Sending Email ..."}
+            </button>
+          </form>
+        ) : (
+          <p className="text-center py-3">
+            Please check your Email - <strong>{userEmail}</strong> for the
+            instructions to reset your ZYCLYX-HRMS Account password
+          </p>
+        )}
       </div>
     </Container>
   );
