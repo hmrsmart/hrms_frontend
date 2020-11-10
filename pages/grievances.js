2ea64f73a8cacc8c2bb33672782ddd5f1389a319
skanjarla <skanjarla@zyclyx.com>
2020-11-10 22:36:16 +0530

    Add section headings

--- pages/grievances.js
+++ pages/grievances.js
@@ -1,17 +1,25 @@
 import React, { useState, useContext, useEffect } from "react";
-import { Container } from "reactstrap";
+import { Container, Form, Button, Input } from "reactstrap";
 import { useForm } from "react-hook-form";
 import Cookie from "js-cookie";
 import { useRouter } from "next/router";
 import AppContext from "../context/AppContext";
 
-const Policies = () => {
+const Grievances = () => {
+  const { register, handleSubmit, errors } = useForm();
+  const token = Cookie.get("token");
+  const appContext = useContext(AppContext);
+  const router = useRouter();
+  const { user } = useContext(AppContext);
+
+  const submitGrievance = () => {};
   return (
     <Container fluid>
       <h1 className="title-text py-3">Grievances</h1>
       {/* Grievance Form */}
       <Container>
         <h2 className="title-text-2">Grievance Form</h2>
+        <Form onSubmit={handleSubmit(submitGrievance)}></Form>
       </Container>
 
       {/* Grievance History */}
@@ -22,4 +30,4 @@ const Policies = () => {
   );
 };
 
-export default Policies;
+export default Grievances;
